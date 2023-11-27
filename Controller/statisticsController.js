5c953266675790a60712de943a2c06390a0ef91c
tushar-454 <imtushar454@gmail.com>
2023-11-27 12:18:49 +0600

    get statistics data based on database so userstatistics and articles statistics api done which give me isPremium yes or not data based on approved article or pending article for that I am using query.

--- Controller/statisticsController.js
+++ Controller/statisticsController.js
@@ -1,4 +1,5 @@
 const Article = require('../Model/Articles');
+const User = require('../Model/User');
 
 const publicationStatistics = async (req, res, next) => {
   const { approvedType } = req.query;
@@ -33,4 +34,61 @@ const publicationStatistics = async (req, res, next) => {
   }
 };
 
-module.exports = { publicationStatistics };
+const usersStatistics = async (req, res, next) => {
+  try {
+    const usersStatistics = await User.aggregate([
+      {
+        $group: {
+          _id: '$isPremium',
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          isPremium: '$_id',
+          count: 1,
+        },
+      },
+    ]);
+    const totalUser = usersStatistics.reduce((acc, cur) => acc + cur.count, 0);
+    res.status(200).json({ totalUser, usersStatistics });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const articleStatistics = async (req, res, next) => {
+  const { approvedType } = req.query;
+  try {
+    const articleStatistics = await Article.aggregate([
+      {
+        $match: {
+          isApprove: approvedType,
+        },
+      },
+      {
+        $group: {
+          _id: '$isPremium',
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          isPremium: '$_id',
+          count: 1,
+        },
+      },
+    ]);
+    const totalArticls = articleStatistics.reduce(
+      (acc, cur) => acc + cur.count,
+      0
+    );
+    res.status(200).json({ totalArticls, articleStatistics });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { publicationStatistics, usersStatistics, articleStatistics };
--- Routes/statistics.js
+++ Routes/statistics.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 const statisticsController = require('../Controller/statisticsController');
 
 router.get('/publications', statisticsController.publicationStatistics);
+router.get('/users', statisticsController.usersStatistics);
+router.get('/aritcles', statisticsController.articleStatistics);
 
 module.exports = router;
